790691d3e62c0202117a06c0c3ea3fa7fb3039d7
vemv <vemv@users.noreply.github.com>
2018-12-27 23:10:46 +0100

    Add dependencies for mp3 playback

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject clj-audio/clj-audio "0.3.0-SNAPSHOT"
   :description "Idiomatic Clojure wrapper for the Java Sound API."
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 ;; https://github.com/pdudits/soundlibs:
+                 [com.googlecode.soundlibs/tritonus-share "0.3.7.4"]
+                 [com.googlecode.soundlibs/jlayer "1.0.1.4"]
+                 [com.googlecode.soundlibs/mp3spi "1.9.5.4"]])
